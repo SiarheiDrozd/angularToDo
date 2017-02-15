fae298389cf5a37a6faa53c541594a95924dbdbb
siarhei_drozd <ggold.d.roger@gmail.com>
2017-02-15 15:48:03 +0300

    something

--- app/columns/columns_controller.js
+++ app/columns/columns_controller.js
@@ -9,25 +9,56 @@ app.directive("tasks", function () {
 
 function Columns( $http ) {
     this.columns = [
-        "TODO",
-        "WIP",
-        "TEST",
-        "DONE"
+        {
+            name: "TODO",
+            items: []
+        },
+        {
+            name: "WIP",
+            items: []
+        },
+        {
+            name: "TEST",
+            items: []
+        },
+        {
+            name: "DONE",
+            items: []
+        }
     ];
     this.data = [];
-    $http.get("data/tasks.json").then(function ( result ) {
-        this.data = result.data;
-        console.log(this);
-    }.bind(this));
+    $http.get("data/tasks.json")
+        .then(function ( result ) {
+            this.data = result.data;
+        }.bind(this))
+        .then(function () {
+            this.data.forEach(function ( task ) {
+                this.add(task);
+            }.bind(this));
+        }.bind(this))
+    ;
 }
+
+Columns.prototype.add = function ( task ) {
+    task.id = this.columns[task.stage].items.length;
+    this.columns[task.stage].items.push(task);
+};
+Columns.prototype.remove = function ( task ) {
+    this.columns[task.stage].items.splice(task.id, 1);
+    this.columns[task.stage].items.forEach(function(item, index){item.id = index});
+};
 
 Columns.prototype.moveLeft = function ( task ) {
     if (task.stage > 0) {
+        this.remove(task);
         task.stage--;
+        this.add(task);
     }
 };
 Columns.prototype.moveRight = function ( task ) {
     if (task.stage < this.columns.length - 1) {
+        this.remove(task);
         task.stage++;
+        this.add(task);
     }
 };
