190f490704b151e75365ddb7fb4fecfbd2c83d2c
Siarhei Drozd <ggold.d.roger@gmail.com>
2017-02-26 20:50:06 +0300

    removed to folder docs for github

--- server.js
+++ server.js
@@ -2,11 +2,11 @@ var express = require('express');
 var path = require("path");
 var app = express();
 
-app.use(express.static(__dirname + "/app/"));
+app.use(express.static(__dirname + "/docs/"));
 
 app.get('/', function (req, res) {
     res.header("Cache-Control", "no-cache, no-store, must-revalidate");
-    res.sendFile(__dirname + "/app/");
+    res.sendFile(__dirname + "/docs/");
 });
 app.listen(3000, function () {
     console.log("listening on 3000");
