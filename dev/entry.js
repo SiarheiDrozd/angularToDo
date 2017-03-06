2d29d804c3e38d9d6012973f266e740ca6f41e52
siarhei_drozd <ggold.d.roger@gmail.com>
2017-03-06 11:46:37 +0300

    routing added

--- dev/entry.js
+++ dev/entry.js
@@ -4,16 +4,39 @@ import TaskCtrl from "./components/task/taskCtrl";
 import ToDoListStorage from "./components/todoList/toDoListStorageService";
 import TaskStateService from "./components/task/taskStateService";
 require("angular-drag-and-drop-lists");
+require("angular-ui-router");
 
+let todoApp = angular.module("ToDoApp", ["ngDragDrop", "ui.router"])
+    .config(function ($stateProvider, $urlRouterProvider) {
+        $urlRouterProvider
+            .otherwise("/todo-list");
+        $stateProvider
+            .state('todoList', {
+                url: "/todo-list",
+                template: "<todo-list></todo-list>"
+            })
+            .state('login', {
+                url: "/login",
+                template: "<login-page></login-page>"
+            });
+    })
 
-let todoApp = angular.module("ToDoApp", ["ngDragDrop"])
     .service("taskStateService", TaskStateService)
     .service("ToDoListStorage", ToDoListStorage)
 
     .controller("todoListCtrl", ["$http", "$scope", "ToDoListStorage", TodoListCtrl])
+    .controller("loginPageCtrl", ["$http", "ToDoListStorage", TodoListCtrl])
     .controller("column", ["taskStateService", "ToDoListStorage", ColumnCtrl])
     .controller("taskCtrl", ["taskStateService", "ToDoListStorage", TaskCtrl])
 
+    .directive("loginPage", function () {
+        return {
+            restrict: "AE",
+            template: require("./components/login/loginPage.html"),
+            controller: "loginPageCtrl",
+            controllerAs: "loginPage"
+        }
+    })
     .directive("todoList", function () {
         return {
             restrict: "AE",
--- server.js
+++ server.js
@@ -6,27 +6,28 @@ var db, Task;
 
 app.use(express.static(__dirname + "/docs/"));
 
-
-let uri = 'mongodb://user1:user1pass@ds157819.mlab.com:57819/angular_todo_db';
-mongoose.Promise = global.Promise;
-mongoose.connect(uri);
-db = mongoose.connection;
-db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', function callback () {
-    let taskSchema = mongoose.Schema({
-        "id": Number,
-        "name": String,
-        "description": String,
-        "stage": Number
+// app.post("/dbConnect", function () {
+    let uri = 'mongodb://user1:user1pass@ds157819.mlab.com:57819/angular_todo_db';
+    mongoose.Promise = global.Promise;
+    mongoose.connect(uri);
+    db = mongoose.connection;
+    db.on('error', console.error.bind(console, 'connection error:'));
+    db.once('open', function callback () {
+        let taskSchema = mongoose.Schema({
+            "id": Number,
+            "name": String,
+            "description": String,
+            "stage": Number
+        });
+        Task = mongoose.model('tasks', taskSchema);
+        let someTask = new Task({
+            "id": 0,
+            "name": "task1",
+            "description": "do something",
+            "stage": 0
+        });
     });
-    Task = mongoose.model('tasks', taskSchema);
-    let someTask = new Task({
-        "id": 0,
-        "name": "task1",
-        "description": "do something",
-        "stage": 0
-    });
-});
+// });
 
 
 app.get('/data', function (req, result) {
@@ -36,7 +37,6 @@ app.get('/data', function (req, result) {
         }
         result.send(data);
     });
-
 });
 
 app.listen(3000, function () {
